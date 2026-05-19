7a8da1d4c459bf01c6798fc92e365455dc1dd057
coura7320-sudo <coura7320@gmail.com>
2026-05-19 12:28:40 +0000

    readme add

--- olympics-frontend/scratch.js
+++
@@ -1,3 +0,0 @@
-const DB = require('c:/Users/coura/OneDrive/Bureau/lab 4 junior 1/server/node_modules/better-sqlite3');
-const db = new DB('./sqlite.db');
-console.log(db.prepare("SELECT name FROM sqlite_master WHERE type='table'").all());
